72adf41571bb7f0e36410a0eae6b46f8054409a0
Cody Howell <codyhowell44@gmail.com>
2026-07-04 21:03:21 -0600

    Prepped for HTTPS, installed auth

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,12 +1,25 @@
+using HowlDev.Web.Authentication.AccountAuth;
+using HowlDev.Web.Authentication.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddAuthService();
+builder.Services.AddLogging();
 
 var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
+
+app.UseAccountIdentityMiddleware(MiddlewareLocation.Headers, options => {
+    options.Paths = ["/api/health"];
+    options.Whitelist = "/api";
+});
 app.UseRouting();
 
-app.MapGet("/api", () => "Hello World!");
+app.MapGet("/api/health", () => "Healthy!");
+
+app.Map("/api/{*rest}", (string rest) => Results.NotFound());
 
 app.MapFallbackToFile("index.html");
 
